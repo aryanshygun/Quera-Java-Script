75108178c2e7783e63595f04a1640c0e80ce2ec3
aryanshygun <ryan.shygun@gmail.com>
2024-08-30 00:49:16 +0330

    commit

--- finished/بهروز و لامپ RGB.js
+++ finished/بهروز و لامپ RGB.js
@@ -1,6 +1,6 @@
-a = 'R'
-b = 'R'
-c = 'B'
+// https://quera.org/college/16059/chapter/64204/lesson/218071/?comments_page=1&comments_filter=ALL
+
+let [ a, b, c] = readline().split(' ')
 
 if ( a == 'R' && b == 'R') {
     console.log("Ghermezo Eshghe!")
