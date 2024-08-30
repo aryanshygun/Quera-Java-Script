d0ce19497adfde6d808537bc8e73fad4f9e188b0
aryanshygun <ryan.shygun@gmail.com>
2024-08-30 14:15:00 +0330

    commit

--- finished/tempCodeRunnerFile.js
+++ finished/tempCodeRunnerFile.js
@@ -1,13 +1,3 @@
-a = 'R'
-b = 'R'
-c = 'B'
-
-if ( a == 'R' && b == 'R') {
-    console.log("Ghermezo Eshghe!")
-} else if ( a == 'B' && b == 'B') {
-    console.log("Abio Eshghe!")
-} else if ( a == 'B' && c == 'B') {
-    console.log("Abio Eshghe!")
-} else {
-    console.log("Doost Nadaram!")
-}
+[x1, y1] = [6, 7]
+// let [x2, y2] = [4, 7]
+// let [x3, 
