1e0dd40774adc2a16620748bcc8564cc66935687
aryanshygun <ryan.shygun@gmail.com>
2024-09-17 10:37:18 +0330

    commit

--- finished/tempCodeRunnerFile.js
+++ finished/tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
-avg, sum
+le (storeNumberOrEnd(readline())) {
+    
+// }
---
+++ finished/تا کی مقلوب.js
@@ -0,0 +1,32 @@
+// https://quera.org/college/16059/chapter/64208/lesson/218138/?comments_page=1&comments_filter=ALL
+
+// const numbersArray = []
+
+// function storeNumberOrEnd(number) {
+//     // TODO
+// }
+
+function printReverse(number) {
+    // let templist = number.toString().split('').map(Number).reverse()
+    // let reversedList = []
+    // for (let i = 0; i < number.length; i++) {
+    //     reversedList.push()
+    // }
+}
+
+// while (storeNumberOrEnd(readline())) {
+    
+// }
+
+
+
+
+
+
+
+x = 34897
+
+let templist = x.toString().split('').map(Number).reverse()
+
+let y = templist.shift()
+console.log(y)
